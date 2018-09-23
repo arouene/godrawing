af0f6b0fe150fce489684ce474704c90affea3b8
Aurelien Rouene <aurelien@devops-now.com>
2018-09-23 23:15:00 +0200

    make a generic polygone generator

--- main.go
+++ main.go
@@ -8,16 +8,21 @@ import (
 	"os"
 )
 
-const length = 100.0
-const border = 10.0
+const (
+	imageWidth    = 3000
+	imageHeight   = 3000
+	length        = 100.0
+	border        = 10.0
+	hexagoneAngle = (1.0 / 3.0) * math.Pi
+)
 
 func main() {
-	img := image.NewRGBA(image.Rect(0, 0, 3000, 3000))
-	drawRectangle(img, 0, 0, 3000, 3000, color.White)
+	img := image.NewRGBA(image.Rect(0, 0, imageWidth, imageHeight))
+	drawRectangle(img, 0, 0, imageWidth, imageHeight, color.White)
 
 	const width = border + length
 	const height = border + length*0.87 // sin(60)
-	const xOffset = width + width*0.5
+	const xOffset = width + width*0.5   // cos(60)
 	const yOffset = height
 
 	// draw two grids of hexagones
@@ -28,28 +33,34 @@ func main() {
 }
 
 func drawGrid(img *image.RGBA, width, height, xOffset, yOffset int, c color.Color) {
-	for i := height + yOffset; i < 3000; i += 2 * height {
-		for j := width + xOffset; j < 3000; j += 3 * width {
+	for i := -height + yOffset; i < imageHeight+height; i += 2 * height {
+		for j := -width + xOffset; j < imageWidth+width; j += 3 * width {
 			drawHexagone(img, j, i, length, c)
 		}
 	}
 }
 
 func drawHexagone(img *image.RGBA, x, y int, l float64, c color.Color) {
-	const angle = (1.0 / 3.0) * math.Pi
-	x0, y0 := nextHexagonePoint(x, y, l, 0)
-	x1, y1 := 0, 0
-	for i := angle; i <= (2*math.Pi)+0.1; i += angle {
-		x1, y1 = nextHexagonePoint(x, y, l, i)
+	nextHexagonePoint := nextPolygonePoint(x, y, l, hexagoneAngle)
+	x0, y0, _ := nextHexagonePoint()
+	x1, y1, _ := nextHexagonePoint()
+	for end := false; !end; x1, y1, end = nextHexagonePoint() {
 		drawLine(img, x0, y0, x1, y1, c)
 		x0, y0 = x1, y1
 	}
 }
 
-func nextHexagonePoint(x, y int, l, t float64) (x0 int, y0 int) {
-	x0 = x + int(l*math.Cos(t))
-	y0 = y + int(l*math.Sin(t))
-	return
+func nextPolygonePoint(x, y int, l, t float64) func() (int, int, bool) {
+	angle := 0.0
+	return func() (x0 int, y0 int, end bool) {
+		x0 = x + int(l*math.Cos(angle))
+		y0 = y + int(l*math.Sin(angle))
+		angle += t
+		if angle > (2*math.Pi)+t+0.1 {
+			end = true
+		}
+		return
+	}
 }
 
 func drawRectangle(img *image.RGBA, x0, y0, x1, y1 int, c color.Color) {
